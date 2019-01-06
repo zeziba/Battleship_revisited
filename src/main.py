e8e24b2a6fb3715f6400a2a64de4aacf1a5558e4
zeziba <owenengen@gmail.com>
2019-01-06 00:02:15 -0700

    Changes:     1: Imported player manger     2: Added check for config file, creates it if not found     3: Added start function         3.a: Logic built to start game     4: Added if __main__ line to run program

--- src/main.py
+++ src/main.py
@@ -1,21 +1,37 @@
-import configmanager
-import uxhandler
 import boardmanager
+import configmanager
 import gamemanager
+import playermanager
+import uxhandler
 
 
 class Battleship():
     def __init__(self):
         config = configmanager.ConfigManager()
-        config.create()
+        try:
+            config.open()
+        except FileNotFoundError:
+            config.create()
         self.config = config.get_config()['settings']
         self.ux = uxhandler.UXHandle(self.config)
-        self.board = boardmanager.BoardManager(self.config, None)
         self.game = gamemanager.GameManager(self.config)
 
     def start(self):
-        pass
+        # Start the game
+        # Ask the player for their name, if empty assume they want to have robots play one another
+        name = self.ux.get_input(self.ux.out["get name"])
+
+        player_board = boardmanager.BoardManager(self.config, name) \
+            if name else boardmanager.BoardManager(self.config, "AI")
+        player = playermanager.Player(name, player_board, self.config) \
+            if name else playermanager.AI(player_board, self.config, playermanager.DIFFICULTY[0])
+
+        ai_board = boardmanager.BoardManager(self.config, "AI")
+        ai = playermanager.AI(ai_board, self.config, playermanager.DIFFICULTY[0])
+
+        self.game.game(player1=player, player2=ai, player1_board=player_board, player2_board=ai_board, ux=self.ux)
 
 
 if __name__ == "__main__":
     b = Battleship()
+    b.start()
