21315a32375dbb02534505228a05f3cff220c4d2
Charles Engen <owenengen@gmail.com>
2021-05-29 13:57:07 -0600

    Changes:   Added tests for Player.py   Added property .destroyed to Player.py
      Moved several things to GamerRules.py
    
      Added docstring to Game.py
      Added init to Game in Game.py
      Added property game to Game in Game.py that yields a player object

--- src/Game.py
+++ src/Game.py
@@ -3,6 +3,7 @@ import src.Board as Board
 import src.Fleet as Fleet
 import src.GameRules as GameRules
 
+
 class Game:
     """
     Create and maintain the differing objects to enable a game of battleship to be played.
@@ -14,12 +15,15 @@ class Game:
 
     The rules of the game are simple.
     """
+
     def __init__(self, players: tuple[bool] = (False, False)) -> None:
         self.state = GameRules.State.STOPPED
         self.players = dict()
         for index, i in enumerate(players):
             state = Player.State.AI if i is False else Player.State.PERSON
-            self.players[f"p{index}"] = Player.Player(state, Board.Board(), Fleet.GeneralFleet())
+            self.players[f"p{index}"] = Player.Player(
+                state, Board.Board(), Fleet.GeneralFleet()
+            )
         print("\n".join([str(p) for p in self.players]))
 
     @property
