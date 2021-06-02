f4591efed13762d58d8287e1cd289eec42969c90
Charles Engen <owenengen@gmail.com>
2021-06-02 13:36:23 -0600

    Updated Game.py to:     Game:__post_init__ to setup the UI attribute and run __set_up     Game:__set_up to set the __player with Player.py:Player objects     Game:set_up to:         Run __set_up as a first step to clear any existing player objects         Automatically generate the fleet if the player is an AI         Go through the actions to allow a Player to place their fleet         Better utilize the GameRules.py

--- src/Game.py
+++ src/Game.py
@@ -1,8 +1,11 @@
-import src.Player as Player
-import src.Board as Board
-import src.Fleet as Fleet
-import src.GameRules as GameRules
 from dataclasses import dataclass, field
+
+import Board as Board
+import Fleet as Fleet
+import GameRules as GameRules
+import Player as Player
+import Ship
+import UI as UI
 
 TESTING = False
 
@@ -25,11 +28,8 @@ class Game:
     state: GameRules.State = field(default=GameRules.State.STOPPED)
 
     def __post_init__(self):
-        for index, i in enumerate(self.players):
-            state = Player.State.AI if i is False else Player.State.PERSON
-            self.__players[f"p{index}"] = Player.Player(
-                state, Board.Board(), Fleet.GeneralFleet()
-            )
+        self.__set_up()
+        self.UI = UI.UI()
 
     @property
     def player(self):
@@ -46,15 +46,56 @@ class Game:
     def stopped(self) -> bool:
         return self.state == GameRules.State.STOPPED
 
+    def __set_up(self) -> None:
+        for index, i in enumerate(self.players):
+            state = Player.State.AI if i is False else Player.State.PERSON
+            self.__players[f"p{index}"] = Player.Player(
+                state, Board.Board(), Fleet.GeneralFleet()
+            )
+
     def set_up(self) -> None:
+        self.__set_up()
         for p in self.player:
             i = 0
             p.board.generate_board()
             p.fleet.generate()
-            # Testing only
             if TESTING:
                 for ship in p.get_ships:
                     ship.place_ship(i := i + 1, 0, p.board)
+            else:
+                if p.state is Player.State.AI:
+                    for ship in p.get_ships:
+                        ship.place_ship(i := i + 1, 0, p.board)
+                else:
+                    ships = [ship for ship in p.get_ships]
+                    size = GameRules.SIZE
+                    while ships:
+                        ship = ships.pop()
+                        print(GameRules.OUTPUTS[3].format(ship.name))
+                        x, y = self.UI.get_selection(GameRules.OUTPUTS[0]).split(" ")
+                        x = int(x)
+                        y = int(y)
+                        h_v = self.UI.get_selection(GameRules.OUTPUTS[1])
+                        if (
+                            not GameRules.check_xy(x, y)
+                            or h_v not in "hv"
+                            or any(
+                                s.contains(px, py)
+                                for s in p.get_ships
+                                for px, py in Ship.Ship.possible_places(
+                                    x, y, s.length, s.directionality
+                                )
+                            )
+                        ):
+                            print(GameRules.OUTPUTS[4].format(ship.name, x, y, h_v))
+                            ships.append(ship)
+                            continue
+                        ship.directionality = (
+                            Ship.Direction.HORIZONTAL
+                            if h_v == "h"
+                            else Ship.Direction.VERTICAL
+                        )
+                        ship.place_ship(x, y, p.board)
         self.start()
 
     def check_win(self) -> bool:
@@ -66,4 +107,5 @@ class Game:
 
 
 if __name__ == "__main__":
-    game = Game((False, False))
+    game = Game((False, True))
+    game.set_up()
