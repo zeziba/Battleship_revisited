05833eae47af092afa3187d966b1520ed1984ee2
Charles Engen <owenengen@gmail.com>
2021-06-02 13:32:15 -0600

    Updated Fleet.py to:     GeneralFleet:other_ships so that any other ship in the GeneralFleet:fleet can be easily given back     GeneralFleet:can_place to better utilize the GameRules.py:check_xy so that the code better follows DRY principle         Reorganized the if statements to better flow so that it was easier to understand where the method was going         Better use of other methods that give the same information as what was being generated in the method

--- src/Fleet.py
+++ src/Fleet.py
@@ -1,9 +1,8 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from enum import Enum, auto
 
 import Ship
 import src.GameRules
-
 
 Fleet = Enum("Fleet", {name: auto() for name in src.GameRules.FLEET})
 FLEET = {ship: src.GameRules.FLEET[ship.name] for ship in list(Fleet)}
@@ -11,7 +10,7 @@ FLEET = {ship: src.GameRules.FLEET[ship.name] for ship in list(Fleet)}
 
 @dataclass
 class GeneralFleet:
-    __fleet = dict()
+    __fleet: dict[Fleet: Ship.Ship] = field(default_factory=dict)
 
     @property
     def fleet(self) -> dict:
@@ -23,8 +22,35 @@ class GeneralFleet:
             # Random directionality choice for now
             self.__fleet[ship] = Ship.Ship(ship.name, FLEET[ship])
 
-    def hit(self, px, py) -> bool:
+    def hit(self, px: int, py: int) -> bool:
         for ship in self.fleet:
             if self.fleet[ship].hit(px, py):
                 return True
+        return False
+
+    def other_ships(self, ship: Fleet) -> iter:
+        for other_ship in self.fleet:
+            if ship is other_ship:
+                continue
+            yield self.fleet[other_ship]
+
+    def can_place(self, ship: Fleet, sx: int, sy: int) -> bool:
+        if src.GameRules.check_xy(sx, sy):
+            x, y = sx, sy
+            if ship.directionality is Ship.Direction.HORIZONTAL:
+                x += ship.length
+            else:
+                y += ship.length
+            if src.GameRules.check_xy(x, y):
+                possible = Ship.Ship.possible_places(
+                    sx, sy, ship.length, ship.directionality
+                )
+                if any(
+                        other.contains(px, py)
+                        for other in self.other_ships(ship)
+                        for px, py in possible
+                ):
+                    return False
+                return True
+            return False
         return False
