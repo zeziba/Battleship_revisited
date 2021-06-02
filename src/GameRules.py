7c14540e84b87355677cc8e49ed372364a717d4e
Charles Engen <owenengen@gmail.com>
2021-06-02 13:37:29 -0600

    Updated GameRules.py.py to:     check_xy was added so that the DRY principle can be better followed     OUTPUTS had several entries added so that better output could be done

--- src/GameRules.py
+++ src/GameRules.py
@@ -2,6 +2,13 @@ from enum import Enum, auto
 
 SIZE = 10
 
+
+def check_xy(x, y) -> bool:
+    """Checks if the x, y coords fall within the board"""
+    size = SIZE - 1
+    return (size >= x >= 0) and (size >= y >= 0)
+
+
 FLEET = {
     "CARRIER": 5,
     "BATTLESHIP": 4,
@@ -9,6 +16,14 @@ FLEET = {
     "SUBMARINE": 3,
     "DESTROYER": 3,
 }
+
+OUTPUTS = (
+    "Please enter in your coords: x y\n\t",
+    "Please enter in your directionality: h -> horizontal or v -> vertical\n\t",
+    "Please enter in the starting location of the {}: x y\n\t",
+    "Placing {}",
+    "Failed to place {} at ({}, {}) with directionality {} as not a valid location.",
+)
 
 
 class State(Enum):
