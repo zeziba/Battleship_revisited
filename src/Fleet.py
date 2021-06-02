f18a1b9bf0e271777d28dbad38859453fab40522
Charles Engen <owenengen@gmail.com>
2021-06-02 13:57:11 -0600

    Updated Fleet.py to:     Fixed name import error

--- src/Fleet.py
+++ src/Fleet.py
@@ -2,10 +2,10 @@ from dataclasses import dataclass, field
 from enum import Enum, auto
 
 import Ship
-import src.GameRules
+import GameRules
 
-Fleet = Enum("Fleet", {name: auto() for name in src.GameRules.FLEET})
-FLEET = {ship: src.GameRules.FLEET[ship.name] for ship in list(Fleet)}
+Fleet = Enum("Fleet", {name: auto() for name in GameRules.FLEET})
+FLEET = {ship: GameRules.FLEET[ship.name] for ship in list(Fleet)}
 
 
 @dataclass
@@ -35,13 +35,13 @@ class GeneralFleet:
             yield self.fleet[other_ship]
 
     def can_place(self, ship: Fleet, sx: int, sy: int) -> bool:
-        if src.GameRules.check_xy(sx, sy):
+        if GameRules.check_xy(sx, sy):
             x, y = sx, sy
             if ship.directionality is Ship.Direction.HORIZONTAL:
                 x += ship.length
             else:
                 y += ship.length
-            if src.GameRules.check_xy(x, y):
+            if GameRules.check_xy(x, y):
                 possible = Ship.Ship.possible_places(
                     sx, sy, ship.length, ship.directionality
                 )
