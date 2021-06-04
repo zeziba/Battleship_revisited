e8013ade45f6061470cb211588c5468a14ba8d20
Charles Engen <owenengen@gmail.com>
2021-06-04 13:47:58 -0600

    Updated Tile.py to:     Updated Tile:__hit dataclasses:field to a default instead of a default_factory     Updated: Tile:contains setter to instead rely on Tile:contains     Updated Tile:has to return Tile:__contains instead of the now changed Tile:contains

--- src/Tile.py
+++ src/Tile.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass, field
 @dataclass
 class Tile:
     __contains: None
-    __hit: False = field(default_factory=bool)
+    __hit: False = field(default=bool)
 
     @property
     def hit(self) -> bool:
@@ -17,14 +17,14 @@ class Tile:
 
     @property
     def contains(self):
-        return self.__contains
+        return self.__contains is not None
 
     @contains.setter
     def contains(self, value):
-        if self.contains is not None:
+        if self.contains is True:
             raise IndexError(f"Location already has {self.contains}")
         self.__contains = value
 
     @property
     def has(self) -> object:
-        return self.contains
+        return self.__contains
