251f4e8acc40e5528f76cd6f3ddc8f51fae0c348
Charles Engen <owenengen@gmail.com>
2021-06-05 13:09:35 -0600

    Updated GameRules.py:     Added EmptyTile     Added HitTile     Updated OUTPUTS to include new outputs

--- src/GameRules.py
+++ src/GameRules.py
@@ -2,8 +2,11 @@ from enum import Enum, auto
 
 SIZE = 10
 
+EmptyTile = ". "
+HitTile = "X "
 
-def check_xy(x, y) -> bool:
+
+def check_xy(x: int, y: int) -> bool:
     """Checks if the x, y coords fall within the board"""
     size = SIZE - 1
     return (size >= x >= 0) and (size >= y >= 0)
@@ -23,6 +26,14 @@ OUTPUTS = (
     "Please enter in the starting location of the {}: x y\n\t",
     "Placing {}",
     "Failed to place {} at ({}, {}) with directionality {} as not a valid location.",
+    "Failed to place {} as input was mangled",
+    "Input must be in teh form of <int> <int> {}",
+    "\nExample:\n1 3",
+    "\nExample:\n<h|v>",
+    "Preparing to take a shot at {}",
+    "Shot at ({},{}) to hit {}",
+    "Coordinates are not valid, attempt again",
+    "Currently turn: {} with player: {} being targeted"
 )
 
 
